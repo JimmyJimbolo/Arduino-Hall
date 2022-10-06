8cd7455179806775509b9d6ae4a9d19950415dbd
Jimmy <101053639+JimmyJimbolo@users.noreply.github.com>
2022-10-06 10:31:53 +0200

    Update WiFi.ino
    Borrado de claves

--- WiFi.ino
+++ WiFi.ino
@@ -1,6 +1,6 @@
 void Wificonnect() {
   WiFi.mode(WIFI_STA);
-  WiFi.begin("Jimbopolis", "aquiles13aquiles13");
+  WiFi.begin("SSID", "PASS");  // SSIS y PASS de tu red WiFi
   while (WiFi.status() != WL_CONNECTED) { delay(200); }
   ip = WiFi.localIP().toString();
 }
