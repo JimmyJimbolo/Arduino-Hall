453bb9f5b26890c68840e1d1ad420f4406679f4e
Jimmy <101053639+JimmyJimbolo@users.noreply.github.com>
2022-10-20 00:29:19 +0200

    Delete HA Hall 2.7 directory

--- HA Hall 2.7/WiFi.ino
+++
@@ -1,6 +0,0 @@
-void Wificonnect() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin("Jimbopolis", "aquiles13aquiles13");
-  while (WiFi.status() != WL_CONNECTED) { delay(200); }
-  ip = WiFi.localIP().toString();
-}
