b16e236c94d89957c0a5137d18be80f3cf96dabd
Jimmy <101053639+JimmyJimbolo@users.noreply.github.com>
2022-10-20 00:30:06 +0200

    Add files via upload

---
+++ HA Hall v2.7/Webhook.ino
@@ -0,0 +1,11 @@
+// https://trigger.macrodroid.com/5041bd55-6eb3-4310-aa9e-0ba623e2bbba/hall? parametro=[parametro]&dato_1=[dato_1];
+
+// void Webhook(String parametro, float dato_1) {
+//   WiFiClientSecure WclientS;
+//   WclientS.setInsecure();
+//   https.begin(WclientS, url + "parametro=" + parametro + "&dato_1=" + String(dato_1));
+//   int httpCode = https.GET();
+//   https.end();
+//   // MQTTout ("arduino/hall/", url+"parametro="+parametro+"&dato_1="+String(dato_1));
+//   // MQTTout ("arduino/hall/", String(httpCode));
+// }
